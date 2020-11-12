a76b72f052c3a8bbfe7f0be47c94f6387246caf7
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2020-11-12 09:54:23 -0300

    componentização
    componentização

--- conceitos/frontend/src/App.js
+++ conceitos/frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
-  return <h1>Hello World!!!</h1>
+  return (
+    <>
+      <Header />
+      <Header />
+    </>
+  );
 }
 
 export default App;
