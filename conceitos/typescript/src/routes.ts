bfa557458190b1a808d79a5a522e48ef8b3e39a2
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-01-05 23:40:21 -0300

    Tipando objetos e vetores
    Tipando objetos e vetores

--- conceitos/typescript/src/routes.ts
+++ conceitos/typescript/src/routes.ts
@@ -1,6 +1,18 @@
-
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWord(request: Request, response: Response) {
+  const user = createUser({
+    name : 'Italo',
+    email: 'italo.og@outlook.com',
+    password: 'fghj',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      { title: 'Javascript', experience: 100},
+    ],
+  }); 
+
   return response.json({message: 'Hello Word'});
 }
