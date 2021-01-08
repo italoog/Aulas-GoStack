99ec573aa3f3fbc10c27292c8367b78193a93ed6
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-01-08 12:46:00 -0300

    eslint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello Word' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🚀 Server started on port 3333!');
 });
