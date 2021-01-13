e9c534f1bafbcc8fb9fb2583f9eeebdede0956d8
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-01-13 12:07:53 -0300

    Repositório do TypeORM

--- primeiro-projeto-node/src/database/migrations/1610503440681-CreateAppointments.ts
+++ primeiro-projeto-node/src/database/migrations/1610503440681-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1610503440681
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
