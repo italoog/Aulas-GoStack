056b0d369d743cb83bd477be8c985445437acb0a
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-01-16 15:07:35 -0300

    Atualizando avatar

--- primeiro-projeto-node/src/config/upload.ts
+++ primeiro-projeto-node/src/config/upload.ts
@@ -2,9 +2,13 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
-  storege: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+  directory: tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
